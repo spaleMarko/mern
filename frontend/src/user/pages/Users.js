23c096b5404e1ff68cf7440ac28a77fa22b9e01a
Marko Spasic <56446513+spaleMarko@users.noreply.github.com>
2020-06-18 00:12:16 +0200

    #2 Done

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-    return <h2>Users Works!</h2>
+    const USERS = [{id: 'u1',
+        name: 'Marko Spasic',
+        image: 'https://lh4.googleusercontent.com/proxy/Du4A4mlP2wW_qYQqReFEWLmuRhk48qVwI43BCuIM2KXDBeV-c1Nbm2ZYwNAsJUASKq0tV-TNVX7VOvksmKNJobokbCzVEX8',
+        places: 10
+    }];
+
+    return <UsersList items={USERS} />
 };
 
 export default Users;
