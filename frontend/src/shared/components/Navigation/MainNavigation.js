f067f14d2dd89fa300d435e49b1bb158cf986618
Marko Spasic <56446513+spaleMarko@users.noreply.github.com>
2020-06-19 14:54:20 +0200

    #4 Done Work

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
+import Backdrop from '../UlElements/Backdrop';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
+    const [ drawerIsOpen , setDrawerIsOpen ] = useState(false);
+
+    const openDrawerHandler = () => {
+        setDrawerIsOpen(true);
+    };
+
+    const closeDrawerHandler = () => {
+        setDrawerIsOpen(false);
+    };
+
     return (
         <React.Fragment>
-            <SideDrawer>
+            {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+
+            <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
                 <nav className="main-navigation__drawer-nav">
                     <NavLinks />
                 </nav>
             </SideDrawer>
+
             <MainHeader>
-                <button className="main-navigation__menu-btn">
+                <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
                     <span />
                     <span />
                     <span />
@@ -23,8 +37,8 @@ const MainNavigation = props => {
                 <h1 className="main-navigation__title">
                     <Link to="/">Your Places</Link>
                 </h1>
-                <nav>
-                    <NavLinks className="main-navigation__header-nav"/>
+                <nav className="main-navigation__header-nav">
+                    <NavLinks />
                 </nav>
             </MainHeader>
         </React.Fragment>
