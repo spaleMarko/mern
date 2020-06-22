486f2dca6c8b5fc76a0dd580f5a1a108d53d6bb6
Marko Spasic <56446513+spaleMarko@users.noreply.github.com>
2020-06-22 16:03:14 +0200

    #9 Add simpel text field

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import Input from '../../shared/components/FormElements/Input';
+import './NewPlace.css';
+
 const NewPlace = () => {
-    return <h2>Places Works!</h2>
+    return <form className="palce-form">
+        <Input element="input" label="Title" />
+    </form>
 };
 
 export default NewPlace;
