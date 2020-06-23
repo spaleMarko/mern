2844ce114615f452141dde18e62014b6a4552297
Marko Spasic <56446513+spaleMarko@users.noreply.github.com>
2020-06-23 21:34:47 +0200

    #14 edit delete logout

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import Card from '../../shared/components/UlElements/Card';
 import Button from '../../shared/components/FormElements/Button';
 import Modal from '../../shared/components/UlElements/Modal';
 import Map from '../../shared/components/UlElements/Map';
+import {AuthContext} from '../../shared/context/auth-context';
 import './PlaceItem.css';
 
 const PlaceItem = props => {
+    const auth = useContext(AuthContext);
+
     const [ showMap, setShowMap] = useState(false);
     const [ showConfirmModal, setShowConfirmModal] = useState(false);
 
@@ -64,8 +67,12 @@ const PlaceItem = props => {
 
                     <div className="place-item__actions">
                         <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
-                        <Button to={`/places/${props.id}`}>EDIT</Button>
-                        <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>
+                        {auth.isLoggedIn && 
+                            <Button to={`/places/${props.id}`}>EDIT</Button>
+                        }
+                        {auth.isLoggedIn && 
+                            <Button danger onClick={showDeleteWarningHandler}>DELETE</Button>
+                        }
                     </div>
                 </Card>
             </li>
