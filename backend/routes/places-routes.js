4504c7f166d83f620a63e94ca1fdee5de5726f2f
Marko Spasic <56446513+spaleMarko@users.noreply.github.com>
2020-06-30 17:47:38 +0200

    #19 Create error with if

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -21,6 +21,15 @@ router.get('/:pid', (req, res, next) => {
     const place = DUMMY_PLACES.find(p => {
         return p.id === placeId;
     });
+
+    if(!place){
+        // throw new Error;
+        // next(error); // Is the same things
+        const error = new Error('Could not find a place for the provided id.');
+        error.code = 404;
+        throw error;
+    }
+
     res.json({place}); // => { place } => { place: place}
 });
 
@@ -29,6 +38,13 @@ router.get('/user/:uid', (req, res, next) => {
     const place = DUMMY_PLACES.find(p => {
         return p.creator === userId;
     });
+
+    if(!place){
+        const error = new Error('Could not find a place for the provided user id.');
+        error.code = 404;
+        return next(error);
+    }
+
     res.json({place}); // => { place } => { place: place}
 });
 
