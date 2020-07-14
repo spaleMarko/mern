2c276d7e46bbaa17ab0fdde3321f8348f34f560b
Marko Spasic <56446513+spaleMarko@users.noreply.github.com>
2020-07-14 23:05:37 +0200

    #26 Added validation for create place

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator');
 
 const placesControllers = require('../controllers/places-controllers');
 
@@ -7,8 +8,21 @@ const router = express.Router();
 // Places routes
 router.get('/:pid', placesControllers.getPlaceById);
 router.get('/user/:uid', placesControllers.getPlacesByUserId);
-router.post('/', placesControllers.createPlace);
+
+router.post(
+    '/',
+    [
+        check('title')
+            .not()
+            .isEmpty(),
+        check('description').isLength({min: 5}),
+        check('address')
+            .not()
+            .isEmpty()
+    ],
+    placesControllers.createPlace);
 router.patch('/:pid', placesControllers.updatePlaceById);
+
 router.delete('/:pid', placesControllers.deletePlace);
 
 module.exports =  router;
