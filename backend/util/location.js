b7996b7f73a55b7d2526e7224b7bac5d3b1220cf
spaleMarko <spasicmarko2010@hotmail.com>
2020-07-28 00:59:42 +0200

    #27 All done

---
+++ backend/util/location.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+
+const HttpError = require('../models/http-error');
+
+const API_KEY = 'AIzaSyCEkwXJaDNmgFXfTVUaC6pz6nnb5iV0yuI';
+
+async function getCoordsForAddress(address){ 
+    // return {
+    //     lat: 40.7484474,
+    //     lng: -73.9871516
+    // };
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`);
+
+    const data = response.data;
+
+    if(!data || data.status === 'ZERO_RESULT'){
+        const error = new HttpError('Could not find location for this specified address', 422);
+        throw error;
+    }
+
+    const coordinates = data.results[0].geometry.location;
+    return coordinates;
+}
+
+module.exports = getCoordsForAddress;
