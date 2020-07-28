8b7fc1dbaa1934d3fa7f328004f8590807122aaf
spaleMarko <spasicmarko2010@hotmail.com>
2020-07-28 15:45:24 +0200

    #31 Create Mongo Client and url

---
+++ mongo_example/mongo.js
@@ -0,0 +1,14 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb+srv://Marko:PQGKmsp7J5qOpF4L@cluster0.25cnz.mongodb.net/places_test?retryWrites=true&w=majority'
+
+const createProduct = async (req, res, next) => {
+
+};
+
+const getProducts = async (req, res, next) => {
+
+};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
