c74b403f0ae161184359ddc4c4cadf28db718db8
spaleMarko <spasicmarko2010@hotmail.com>
2020-07-28 22:35:49 +0200

    #31 Get all products 5.

--- mongo_example/mongo.js
+++ mongo_example/mongo.js
@@ -23,7 +23,17 @@ const createProduct = async (req, res, next) => {
 };
 
 const getProducts = async (req, res, next) => {
-
+    const client = new MongoClient(url);
+    let products;
+    try{
+        await client.connect();
+        const db = client.db();
+        products = await db.collection('products').find().toArray();
+    }catch(error){
+        return res.json({message: 'Coluld not retrive products'})
+    }
+    client.close();
+    res.json(products);
 };
 
 exports.createProduct = createProduct;
