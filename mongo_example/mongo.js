b08bf9f8509aedc7715969c3c720f57e082026b3
spaleMarko <spasicmarko2010@hotmail.com>
2020-07-28 16:05:19 +0200

    #31 Create new product 4.

--- mongo_example/mongo.js
+++ mongo_example/mongo.js
@@ -3,7 +3,23 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb+srv://Marko:PQGKmsp7J5qOpF4L@cluster0.25cnz.mongodb.net/places_test?retryWrites=true&w=majority'
 
 const createProduct = async (req, res, next) => {
+    const newProduct = {
+        name: req.body.name,
+        price: req.body.price
+    };
 
+    const client = new MongoClient(url);
+
+    try{
+        await client.connect();
+        const db = client.db();
+        const result = db.collection('products').insertOne(newProduct);
+    }catch(error){
+        return res.json({message: 'Could not store data'})
+    };
+
+    client.close();
+    res.json(newProduct);
 };
 
 const getProducts = async (req, res, next) => {
