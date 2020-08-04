050c00ce46c2c02cf224e2ca4ab3d502cfa4575a
spaleMarko <spasicmarko2010@hotmail.com>
2020-08-05 00:25:01 +0200

    #49 Add Signup fetch fix cors error

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,14 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// For cors error
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+});
+
 app.use('/api/places', placesRouter); // => /api/places/...
 app.use('/api/users', usersRouter); // => /api/users/...
 
