9392e010ab0637739b8bd8fbe71568b4fa44ae5d
spaleMarko <spasicmarko2010@hotmail.com>
2020-08-10 17:09:20 +0200

    #49 Implements custom hook to users.js

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -3,41 +3,29 @@ import React, { useEffect, useState } from 'react';
 import UsersList from '../components/UsersList';
 import ErrorModal from '../../shared/components/UlElements/ErrorModal';
 import LoadingSpiner from '../../shared/components/UlElements/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 const Users = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState();
+    const {isLoading, error, sendRequest, clearError} = useHttpClient();
     const [loadedUsers, setLoadedUsers] = useState();
 
     useEffect(() => {
-        const sendRequest = async () => {
-            setIsLoading(true);
+        const fetchUsers = async () => {
             try{
-                const response = await fetch('http://localhost:5000/api/users');
-                const responseData = await response.json();
-                
-                if(!response.ok){
-                    throw new Error(responseData.message);
-                }
-
+                const responseData = await sendRequest('http://localhost:5000/api/users');
+    
                 setLoadedUsers(responseData.users);
                 
             }catch(err){
-                setIsLoading(false);
-                setError(err.message);
+               
             }
-            setIsLoading(false);
         };
-        sendRequest();
-    }, []);
-
-    const errorHandler = () => {
-        setError(null);
-    };
+        fetchUsers();
+    }, [sendRequest]);
 
     return( 
         <React.Fragment>
-            <ErrorModal error={error} onClear={errorHandler} />
+            <ErrorModal error={error} onClear={clearError} />
             {isLoading && (
                 <div className="center">
                     <LoadingSpiner />
