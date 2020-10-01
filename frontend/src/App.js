8574f3c9c83cd63377107a1166783e197d55d2ff
spaleMarko <spasicmarko2010@hotmail.com>
2020-10-01 23:35:44 +0200

    #56 1. Basic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,8 +14,6 @@ const App = () => {
   const [ token, setToken] = useState(false);
   const [ userId, setUserId] = useState(false);
 
-  
-
   const login = useCallback((uid, token) => {
     setToken(token);
     setUserId(uid);
@@ -28,7 +26,15 @@ const App = () => {
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    localStorage.removeItem('userData');
   }, []);
+
+  useEffect( () => {
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    if (storedData && storedData.token){
+      login(storedData.userId, storedData.token);
+    }
+  }, [login]);
 
   let routes;
 
