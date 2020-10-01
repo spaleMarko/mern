3439df9fb6eb26cc87cd6502e76597911d217c76
spaleMarko <spasicmarko2010@hotmail.com>
2020-10-01 16:16:49 +0200

    #54 Added protection to routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,13 +2,16 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const placesControllers = require('../controllers/places-controllers');
-const fileUpload = require('../middleware/file-upload')
+const fileUpload = require('../middleware/file-upload');
+const checkAuth = require('../middleware/check-auth');
 
 const router = express.Router();
 
 // Places routes
 router.get('/:pid', placesControllers.getPlaceById);
 router.get('/user/:uid', placesControllers.getPlacesByUserId);
+
+router.use(checkAuth);
 
 router.post(
     '/',
