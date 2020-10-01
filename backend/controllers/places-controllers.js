dc9f47eb1292696f889f51d3592dcbce8231836b
spaleMarko <spasicmarko2010@hotmail.com>
2020-10-01 17:34:26 +0200

    #54 Added token and protection for update place cant update diferent users

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -137,6 +137,14 @@ const updatePlaceById = async (req, res, next) => {
         return next(error);
     }
 
+    if (place.creator.toString() !== req.userData.userId){
+        const error = new HttpError(
+            'You are not allowed to edit this place',
+            401
+        );
+        return next(error);
+    }
+
     place.title = title;
     place.description = description;
 
